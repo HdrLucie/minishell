2620b15e69c9a115012b2c5b4be7940da4e98470
Luciehdr <houdeyerlucie@hotmail.fr>
2021-10-18 12:51:54 +0200

    export multi var fix

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: hlucie <hlucie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/28 14:28:50 by hlucie            #+#    #+#             */
-/*   Updated: 2021/10/18 12:11:45 by hlucie           ###   ########.fr       */
+/*   Updated: 2021/10/18 12:48:33 by hlucie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,78 +31,64 @@ int	change_exp_value(t_env *env, char *name_exp, char *value_exp)
 	return (0);
 }
 
-void	fill_value_exp(char **value, char **cmd, int j)
+void	fill_value_exp(char *cmd, char **value, int i)
 {
-	int	i;
 	int	k;
 
-	i = 1;
 	k = 0;
-	while (cmd[i][j] && (cmd[i][j] == '=' || cmd[i][j] == '\"' || cmd[i][j] =='\''))
-		j++;
-	j--;
+	while (cmd[i] && (cmd[i] == '=' || cmd[i] == '\"' || cmd[i] =='\''))
+		i++;
 	while(cmd[i])
 	{
-		while (cmd[i][++j])
-		{
-			if (cmd[i][j] != '\"' && cmd[i][j] != '\'')
-				(*value)[k++] = cmd[i][j];
-		}
-		j = -1;
+		if (cmd[i] != '\"' && cmd[i] != '\'')
+			(*value)[k++] = cmd[i];
 		i++;
 	}
 	(*value)[k] = '\0';
 }
 
-int	recover_value_exp(char **cmd, char **value, int j)
+int	recover_value_exp(char *cmd, char **value, int i)
 {
-	int	i;
 	int	counter_l;
 	int	tmp_index;
 
-	i = 1;
 	counter_l = 0;
-	tmp_index = j;
+	tmp_index = i;
 	while(cmd[i])
 	{
-		while (cmd[i][++j])
-		{
-			if (cmd[i][j] != '\"' || cmd[i][j] != '=' || cmd[i][j] != '\'')
+		if (cmd[i] != '\"' || cmd[i] != '=' || cmd[i] != '\'')
 				counter_l++;
-		}
-		j = -1;
 		i++;
 	}
 	*value = malloc(sizeof(char) * counter_l + 2);
 	if (!value)
 		return (-1);
-	fill_value_exp(value, cmd, tmp_index);
+	fill_value_exp(cmd, value, tmp_index);
 	return (0);
 }
 
-int	recover_name_exp(char **cmd, char **name, char **value)
+int	recover_name_exp(char *cmd, char **name, char **value)
 {
 	int	i;
-	int	j;
 	int	k;
 	int	counter_l;
 
-	i = 1;
-	j = -1;
+	i = -1;
 	k = 0;
 	counter_l = 0;
-	if (!cmd[1])
+	(void)value;
+	if (!cmd)
 		return (-2);
-	while (cmd[i][++j] && cmd[i][j] != '=')
+	while (cmd[++i] && cmd[i] != '=')
 		counter_l++;
 	*name = malloc(sizeof(char) * counter_l + 1);
 	if (!name)
 		return (-1);
-	j = -1;
-	while (cmd[i][++j] && cmd[i][j] != '=')
-		(*name)[k++] = cmd[i][j];
+	i = -1;
+	while (cmd[++i] && cmd[i] != '=')
+		(*name)[k++] = cmd[i];
 	(*name)[k] = '\0';
-	recover_value_exp(cmd, value, j);
+	recover_value_exp(cmd, value, i);
 	return (0);
 }
 
@@ -125,21 +111,20 @@ int	export_var(t_env *env, char **var_export)
 
 	value_exp = NULL;
 	name_exp = NULL;
-	// int	i = 0;
-	// while (var_export[i])
-	// {
-	// 	printf("EXPORT : %s\n", var_export[i]);
-	// 	i++;
-	// }
+	int	i = 1;
 	if (var_export && !var_export[1])
 	{
 		udpate_alpha_road(env);
 		return (0);
 	}
-	recover_name_exp(var_export, &name_exp, &value_exp);
-	if (!check_env(env, name_exp))
-		create_export_node(env, name_exp, value_exp);
-	else
-		change_exp_value(env, name_exp, value_exp);
+	while (var_export[i])
+	{
+		recover_name_exp(var_export[i], &name_exp, &value_exp);
+		if (!check_env(env, name_exp))
+			create_export_node(env, name_exp, value_exp);
+		else
+			change_exp_value(env, name_exp, value_exp);
+		i++;
+	}
 	return (1);
 }
--- test/tester.sh
+++ test/tester.sh
@@ -162,6 +162,8 @@ exec_test '< /dev/null cat'
 ENV_SHOW="env"
 EXPORT_SHOW="export"
 exec_env 'export'
+exec_env 'export coucou="salut les gars" hey="heyhey"'
+exec_env 'export coucou="salut" blabla=hey'
 # exec_test 'export 1TEST=louloute ;' $ENV_SHOW
 # exec_test 'export TEST ;' $EXPORT_SHOW
 # exec_test 'export ""="" ; ' $ENV_SHOW
