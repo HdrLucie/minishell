4297e0555e784fee405df525db16335fcb464688
Luciehdr <houdeyerlucie@hotmail.fr>
2021-10-27 14:13:30 +0200

    test

--- srcs/pipe/pipe.c
+++ srcs/pipe/pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ehautefa <ehautefa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hlucie <hlucie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 12:22:31 by ehautefa          #+#    #+#             */
-/*   Updated: 2021/10/27 12:50:35 by ehautefa         ###   ########.fr       */
+/*   Updated: 2021/10/27 14:11:13 by hlucie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 int	execute_pipe(int *fd, int fd_std, int pid)
 {
-	printf("PID : %d, fd[fd_std] : %d %d\n",pid, fd[fd_std], fd[!fd_std]);
+	(void)pid;
+	// printf("PID : %d, fd[fd_std] : %d %d\n",pid, fd[fd_std], fd[!fd_std]);
 	close(fd[fd_std]);
 	if (dup2(fd[!fd_std], fd_std) == -1)
 		return (print_error(strerror(errno), -1));
 	close(fd[!fd_std]);
-	printf("PID : %d, fd[fd_std] : %d %d\n", pid, fd[fd_std], fd[!fd_std]);
+	// printf("PID : %d, fd[fd_std] : %d %d\n", pid, fd[fd_std], fd[!fd_std]);
 	return (0);
 }
 
@@ -29,18 +30,18 @@ int	exe_pipe(t_mini *mini, t_cmd *cmd)
 	int	ret;
 
 	ret = 0;
-	(void)mini;
-	if (cmd->pipe_in)
-		printf("IN : 0 : %d 1: %d\n", cmd->pipe_in[0], cmd->pipe_in[1]);
-	if (cmd->pipe_out)
-		printf("OUT : 0 : %d 1: %d\n", cmd->pipe_out[0], cmd->pipe_out[1]);
+	// (void)mini;
+	// if (cmd->pipe_in)
+	// 	printf("IN : 0 : %d 1: %d\n", cmd->pipe_in[0], cmd->pipe_in[1]);
+	// if (cmd->pipe_out)
+	// 	printf("OUT : 0 : %d 1: %d\n", cmd->pipe_out[0], cmd->pipe_out[1]);
 	if (cmd->pipe_out)
 	{
 		pid = fork();
 		if (pid == 0)
 		{
-			printf("SAlut c'est moi Lucette \n\n");
-			printf("FCNTL of %d : %d\n", cmd->pipe_out[0], fcntl(cmd->pipe_out[0], 0, F_GETFL));
+			// printf("SAlut c'est moi Lucette \n\n");
+			// printf("FCNTL of %d : %d\n", cmd->pipe_out[0], fcntl(cmd->pipe_out[0], 0, F_GETFL));
 			exe_pipe(mini, cmd->next);
 			exit(0);
 		}
@@ -54,7 +55,7 @@ int	exe_pipe(t_mini *mini, t_cmd *cmd)
 	ret = redir(cmd->cmd, mini);
 	if (ret == -1)
 		return (-1);
-			printf(" PID : %d SAlut c'est moi Ginette \n\n", pid);
+			// printf(" PID : %d SAlut c'est moi Ginette \n\n", pid);
 
 	waitpid(pid, NULL, 0);
 	return (ret);
